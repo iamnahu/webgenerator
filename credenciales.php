6674bb0483c186cbe77945433448006c57cc7cd8
iamnahu <118862380+iamnahu@users.noreply.github.com>
2022-11-23 00:19:59 -0300

    Edite credenciales

--- credenciales.php
+++ credenciales.php
@@ -1,6 +1,6 @@
 <?php
 	define("HOST", "localhost");
-	define("USER", "3805");
-	define("PASS", "3805");
-	define("DB", "3805");
+	define("USER", "adm_webgenerator");
+	define("PASS", "webgenerator2020");
+	define("DB", "webgenerator");
 ?>
